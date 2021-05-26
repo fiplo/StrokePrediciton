e1816ae64c9488999be69cab551de22d27abf481
Tadas552 <tadasdulke5@gmail.com>
2021-05-26 22:59:39 +0300

    initial commit, added reading, basic file structure

---
+++ main.py
@@ -0,0 +1,10 @@
+from shared import *
+
+
+def main():
+    data = read_file('./data/healthcare-dataset-stroke-data.csv')
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ shared/read_file.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def read_file(file_name):
+    return pd.read_csv(file_name)
