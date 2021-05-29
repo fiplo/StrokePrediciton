93cfdfddbb1d18b2531411e2a1d150c59fdccdba
paurat <paulius.ratkevicius@ktu.lt>
2021-05-29 22:53:44 +0300

    Implemented func for data normalization changed parsing so ever married is either 1 or 0

--- shared/read_file.py
+++ shared/read_file.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sklearn import preprocessing
 
 def read_file(file_name):
     strokeData = pd.read_csv(file_name)
@@ -7,4 +7,15 @@ def read_file(file_name):
     del strokeData['id']
     strokeData = strokeData.dropna()
     strokeData = strokeData[strokeData.smoking_status != 'Unknown']
+    strokeData = strokeData.replace(to_replace=['Yes', 'No'], value=[1, 0])
     return strokeData
+
+def normalize(df):
+    min_max_scaler = preprocessing.MinMaxScaler()
+    column_names_to_normalize = ['age', 'avg_glucose_level', 'bmi']
+    x = df[column_names_to_normalize].values
+    x_scaled = min_max_scaler.fit_transform(x)
+    df_temp = pd.DataFrame(x_scaled, columns=column_names_to_normalize, index = df.index)
+    df[column_names_to_normalize] = df_temp
+    return df
+
