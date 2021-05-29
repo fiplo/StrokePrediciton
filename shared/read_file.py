109128ae2a012cd65574c2b52d685a7cc0703634
Tadas Dulke <tadasdulke5@gmail.com>
2021-05-29 23:15:52 +0300

    residence_type and smoking_status normalization

--- shared/read_file.py
+++ shared/read_file.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn import preprocessing
 
+
 def read_file(file_name):
     strokeData = pd.read_csv(file_name)
     strokeData = strokeData.reset_index().drop('index', axis=1)
@@ -9,14 +10,16 @@ def read_file(file_name):
     strokeData = strokeData[strokeData.smoking_status != 'Unknown']
     strokeData = strokeData.replace(to_replace=['Yes', 'No'], value=[1, 0])
     strokeData = strokeData.replace(to_replace=['Male', 'Female', 'Other'], value=[1, 0, 0.5])
+    strokeData = strokeData.replace(to_replace=['smokes', 'never smoked', 'formerly smoked'], value=[1, 0, 0.5])
+    strokeData = strokeData.replace(to_replace=['Urban', 'Rural'], value=[1, 0])
     return strokeData
+
 
 def normalize(df):
     min_max_scaler = preprocessing.MinMaxScaler()
     column_names_to_normalize = ['age', 'avg_glucose_level', 'bmi']
     x = df[column_names_to_normalize].values
     x_scaled = min_max_scaler.fit_transform(x)
-    df_temp = pd.DataFrame(x_scaled, columns=column_names_to_normalize, index = df.index)
+    df_temp = pd.DataFrame(x_scaled, columns=column_names_to_normalize, index=df.index)
     df[column_names_to_normalize] = df_temp
     return df
-
