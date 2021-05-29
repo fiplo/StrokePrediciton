ffba3715b8bbaf3b0118615fa977fe06f220b44d
paurat <paulius.ratkevicius@ktu.lt>
2021-05-29 19:03:11 +0300

    Removed empty data and index column was removed

--- shared/read_file.py
+++ shared/read_file.py
@@ -2,4 +2,9 @@ import pandas as pd
 
 
 def read_file(file_name):
-    return pd.read_csv(file_name)
+    strokeData = pd.read_csv(file_name)
+    strokeData = strokeData.reset_index().drop('index', axis=1)
+    del strokeData['id']
+    strokeData = strokeData.dropna()
+    strokeData = strokeData[strokeData.smoking_status != 'Unknown']
+    return strokeData
