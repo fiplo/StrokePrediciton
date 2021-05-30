b850cca7c3a2552bd3cd5e7d6e8a148b8e932958
paurat <paulius.ratkevicius@ktu.lt>
2021-05-30 19:13:03 +0300

    Plot functions for Kmeans

--- Kmeans/main.py
+++ Kmeans/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn import metrics
+import numpy as np
+from yellowbrick.cluster import SilhouetteVisualizer
+import matplotlib.pyplot as plt
 
 kmeans_kwargs = {
     "init": "random",
@@ -27,4 +30,15 @@ def kmeanSil(data, numarr):
         sc.append(score)
     return sc
 
+def plotKmeans(twoDimensionalData, labels, ax)
+    u_labels = np.unique(label)
+    for i in u_labels:
+        ax.scatter(twoDimensionalData[labels == i][twoDimensionalData.columns[0][0]] , twoDimensionalData[label == i][twoDimensionalData.columns[1][0]] , label = i)
+    ax.set(xlabel=twoDimensionalData.columns[0][0], ylabel=twoDimensionalData.columns[1][0])
+    ax.set_title(f"{len(u_labels)} clusters")
+
+def SilhouetteKmeansPlot(twoDimensionalData, n, ax):
+    kmeans = returnKmeanClusters(twoDimensionalData, n)
+    visualizer = SilhouetteVisualizer(kmeans, colors='yellowbrick', ax=ax)
+    visualizer.fit(twoDimensionalData)
 
